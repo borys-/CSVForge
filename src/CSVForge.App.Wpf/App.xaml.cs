9ab387ae98340fa0fee24447749a592c0d9eb7d3
BorysPatyk <borys.patyk@gmail.com>
2026-07-22 19:29:11 +0200

    Add dependency injection bootstrap

--- src/CSVForge.App.Wpf/App.xaml.cs
+++ src/CSVForge.App.Wpf/App.xaml.cs
@@ -1,13 +1,45 @@
-﻿using System.Configuration;
-using System.Data;
+using CSVForge.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
+using ApplicationDependencyInjection = CSVForge.Application.DependencyInjection;
 
 namespace CSVForge.App.Wpf;
 
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
-}
+    private readonly IHost _host;
 
+    public App()
+    {
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                ApplicationDependencyInjection.AddApplication(services);
+                services.AddInfrastructure();
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await _host.StartAsync();
+
+        MainWindow mainWindow = _host.Services.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await _host.StopAsync();
+        _host.Dispose();
+
+        base.OnExit(e);
+    }
+}
--- src/CSVForge.Cli/Program.cs
+++ src/CSVForge.Cli/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CSVForge.Application;
+using CSVForge.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddApplication();
+        services.AddInfrastructure();
+    })
+    .Build();
+
+IHostApplicationLifetime lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
+
+Console.WriteLine("CSVForge CLI");
+lifetime.StopApplication();
